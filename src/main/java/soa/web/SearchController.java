6bc753c18bcde3a8e0ce00080cede247aabd49e1
danirueda <danir94@gmail.com>
2016-12-07 09:43:02 +0100

    Query interface extended and twitter keys added.

--- src/main/java/soa/web/SearchController.java
+++ src/main/java/soa/web/SearchController.java
@@ -7,6 +7,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.HashMap;
+import java.util.Map;
+
 
 @Controller
 public class SearchController {
@@ -23,6 +26,14 @@ public class SearchController {
     @RequestMapping(value="/search")
     @ResponseBody
     public Object search(@RequestParam("q") String q) {
-        return producerTemplate.requestBodyAndHeader("direct:search", "", "CamelTwitterKeywords", q);
+	    int index = q.lastIndexOf("max:");
+	    Map<String,Object> headers = new HashMap<>();
+	    if(index != -1){
+	        headers.put("CamelTwitterCount",Integer.parseInt(q.substring(index+4)));
+            headers.put("CamelTwitterKeywords",q.substring(0,index-1));
+        } else {
+	        headers.put("CamelTwitterKeywords",q);
+        }
+        return producerTemplate.requestBodyAndHeaders("direct:search","",headers);
     }
 }
